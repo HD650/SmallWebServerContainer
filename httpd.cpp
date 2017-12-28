edda5f54d3a0ee7097b19fc4665213fcc4371518
skywhat <i@skywhat.com>
2017-12-28 18:32:45 -0500

    parse_url function to serve static files

--- httpd.cpp
+++ httpd.cpp
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <regex>
 
 using namespace std;
 
-bool return_file(int client, char* file_name)
+void return_file(int client, char* file_name)
 {
 	FILE* return_file=fopen(file_name,"r");
 	if(return_file==NULL)
@@ -30,6 +31,59 @@ bool return_file(int client, char* file_name)
 	}
 }
 
+int get_line(int sock, char *buf, int size)
+{
+    int i = 0;
+    char c = '\0';
+    int n;
+
+    while ((i < size - 1) && (c != '\n'))
+    {
+        n = recv(sock, &c, 1, 0);
+        /* DEBUG printf("%02X\n", c); */
+        if (n > 0)
+        {
+            if (c == '\r')
+            {
+                n = recv(sock, &c, 1, MSG_PEEK);
+                /* DEBUG printf("%02X\n", c); */
+                if ((n > 0) && (c == '\n'))
+                    recv(sock, &c, 1, 0);
+                else
+                    c = '\n';
+            }
+            buf[i] = c;
+            i++;
+        }
+        else
+            c = '\n';
+    }
+    buf[i] = '\0';
+
+    return(i);
+}
+
+void parse_url(char* method,char* path,int sock_id){
+	char buf[2048];
+	size_t i=0,j=0;
+	size_t num_char;
+	num_char=get_line(sock_id,buf,sizeof(buf));
+	while(buf[i]!=' '&&(i<num_char)){
+		method[i]=buf[i];
+		i++;
+	}
+	method[i]='\0';
+	j=i;
+	i=0;
+	while(buf[j]==' '&&(j<num_char)){
+		j++;
+	}
+	while(buf[j]!=' '&&(j<num_char)){
+			path[i]=buf[j];
+			i++;j++;
+	}
+}
+
 int main()
 {
 	printf("Start a socket.\n");
@@ -39,7 +93,7 @@ int main()
 	{
 		printf("error in creating a socket.\n");
 		return 1;
-	}	
+	}
 	struct sockaddr_in serv_addr;
 	int port_num=4000;
 	serv_addr.sin_family=AF_INET;
@@ -59,9 +113,13 @@ int main()
 	while(1)
 	{
 		new_sock_id=accept(sock_id,(struct sockaddr*)&cli_addr,&cli_length);
+		char method[255],path[512];
+		parse_url(method,path,new_sock_id);
+		//printf("method: %s\n",method);
+		//printf("path: %s\n",path);
 		char *ip = inet_ntoa(cli_addr.sin_addr);
-		printf("client ip:%s\n",ip);
-		printf("%d\n",cli_addr.sin_port);
+		//printf("client ip:%s\n",ip);
+		//printf("%d\n",cli_addr.sin_port);
 		char buf[1024];
 		strcpy(buf,"HTTP/1.0 200 OK\r\n");
 		send(new_sock_id,buf,strlen(buf),0);
@@ -69,9 +127,9 @@ int main()
 		send(new_sock_id, buf, strlen(buf), 0);
 		strcpy(buf, "\r\n");
 		send(new_sock_id, buf, strlen(buf), 0);
-		return_file(new_sock_id,"index.html");
+		return_file(new_sock_id,&path[1]);
 		close(new_sock_id);
-	}	
+	}
 	close(sock_id);
 	return 0;
 }
