7fee2f69308631662f868cb416cd8241be662df6
HD650 <zzhang63@ncsu.edu>
2018-01-03 00:11:24 -0500

    add a threadpool to speed up the context switch

---
+++ ThreadPool.cpp
@@ -0,0 +1,80 @@
+#include "ThreadPool.h"
+
+ThreadPool::ThreadPool(int num_thread)
+{
+	for(int i=0;i<num_thread;i++)
+	{
+		threads.push_back(new thread(std::bind(&ThreadPool::inner_loop,this)));
+	}
+	printf("pooling finished\n");
+	global_count=0;
+}
+
+bool ThreadPool::start_pool()
+{
+
+	return true;
+}
+
+bool ThreadPool::add_job(void*(*job_function)(void*),void* job_param)
+{
+	pool_mutex.lock();
+	global_count++;
+	struct Job* temp=new struct Job(job_function,job_param,global_count);
+	job_list.push_back(temp);
+	if(job_list.size()==1)
+	{
+		empty_mutex.unlock();
+	}
+	pool_mutex.unlock();
+}
+
+bool ThreadPool::shutdown()
+{
+
+	for(auto i=threads.begin();i!=threads.end();i++)
+	{
+
+	}
+}
+
+void ThreadPool::join_pool()
+{
+	while(1)
+	{
+		pool_mutex.lock();
+		if(job_list.empty())
+		{
+			return;
+		}
+		else
+		{
+			sleep(1);
+		}
+		pool_mutex.unlock();
+	}
+}
+
+void ThreadPool::inner_loop()
+{
+	while(1)
+	{
+		pool_mutex.lock();
+		if(job_list.empty())
+		{
+			pool_mutex.unlock();
+			empty_mutex.lock();
+		}
+		else
+		{
+			//if the job list is not empty, call a job
+			struct Job* temp=job_list.back();
+			job_list.pop_back();
+			pool_mutex.unlock();
+			(*temp)();
+			delete temp;
+		}
+	}
+	return ;
+}
+
---
+++ ThreadPool.h
@@ -0,0 +1,54 @@
+#ifndef THREADPOOL__H
+#define THREADPOOL__H
+
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <list>
+#include <functional>
+#include <time>
+
+using namespace std;
+
+struct Job
+{
+	void* (*job_function)(void*);
+	void* job_param;
+	int job_id;
+
+	Job(void* (*_job_function)(void*),void* _job_param,int _job_id)
+	{
+		job_function=_job_function;
+		job_param=_job_param;
+		job_id=_job_id;
+	}
+
+	//we can just call the struct to run the job
+	void operator()()
+	{
+		job_function(job_param);
+	}
+};
+
+class ThreadPool
+{
+public:
+	ThreadPool(int num_thread);
+	bool start_pool();
+	bool add_job(void*(*job_function)(void*),void* job_param);
+	bool shutdown();
+	//this will wait until the pool finish all the jobs
+	void join_pool();
+private:
+	int global_count;
+	//save the pointer of job to speed up the assignment
+	list<struct Job*> job_list;
+	list<thread*> threads;
+	//mutex to control accessing of pool and accessing if contion variable
+	mutex pool_mutex;
+	mutex empty_mutex;
+	//main loop for all threads in pool
+	void inner_loop();
+};
+
+#endif
